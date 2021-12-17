036b4427049112c76382f363feed6da0ac4aa299
ewanouchka <ewanouchka@gmail.com>
2021-12-17 02:01:12 +0100

    ajout route profile

--- grouporama-app/src/router/index.js
+++ grouporama-app/src/router/index.js
@@ -21,14 +21,17 @@ const routes = [
   {
     path: "/signup",
     name: "Signup",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "signup" */ "../views/Signup.vue"),
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () => import(/* webpackChunkName: "profile" */ "../components/Profile.vue"),
   },
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes,
 });
 
