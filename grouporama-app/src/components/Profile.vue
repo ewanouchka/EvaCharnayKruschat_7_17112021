6d08e20da7fc5df6ab5fcaf593dd563c1143a93c
ewanouchka <ewanouchka@gmail.com>
2021-12-17 02:09:33 +0100

    Base page profil à remplir

---
+++ grouporama-app/src/components/Profile.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="profile">
+    <h1>{{ msg }}</h1>
+    <p>
+      UC
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
