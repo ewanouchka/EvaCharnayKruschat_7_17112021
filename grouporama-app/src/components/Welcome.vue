7e44e90fcbbdc4f042a6dfc84af8fa9e03acc999
ewanouchka <ewanouchka@gmail.com>
2021-12-21 12:40:33 +0100

    démarrage bloc bienvenue

---
+++ grouporama-app/src/components/Welcome.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="welcome">
+    <h1>{{ msg }}</h1>
+    <p>
+      UC
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  /*props: {
+    msg: String
+  },*/
+
+  data: 
+    function() {
+      return {
+        msg: "Welcome",
+      };
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
