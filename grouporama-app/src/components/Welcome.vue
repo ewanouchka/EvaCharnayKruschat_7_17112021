4ed2d26643454c4c619820d85dde6ab8f1872028
ewanouchka <ewanouchka@gmail.com>
2021-12-27 13:48:16 +0100

    démarrage accès infos utilisateurs

--- grouporama-app/src/components/Welcome.vue
+++ grouporama-app/src/components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="welcome">
-    <h1>{{ msg }} {{ userSurname }}</h1>
+    <h1>{{ welcome }} {{ authentifiedUser }} !</h1>
     <div>
       Voici les dernières actualités :
       <div id="welcome__actu">bloc dernier message à voir</div>
@@ -10,35 +10,58 @@
 
 <script>
 export default {
-  name: 'Welcome',
-  /*props: {
-    msg: String
-  },*/
+  name: "Welcome",
+  props: ["userSurname"],
+  data: function () {
+    return {
+      welcome: "Bienvenue",
+      authentifiedUser: this.userSurname,
+    };
+  },
+  methods: {
+    getUserName() {
+      const userToken = JSON.parse(localStorage.getItem("userAuth")).token;
+      (async () => {
+        try {
+          const userProfile = await fetch("http://localhost:3000/api/profile", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${userToken}`,
+            },
+          });
+          const userProfileJSON = await userProfile.json();
 
-  data: 
-    function() {
-      return {
-        msg: "Bienvenue",
-        userSurname: "inconnu", // revoir l'appel du userName avec le profil
-      };
+          if (userProfileJSON) {
+            this.authentifiedUser = userProfileJSON.first_name;
+            return;
+          }
+        } catch (error) {
+          console.log("impossible d'accéder au profil utilisateur");
+        }
+      })();
     },
-}
+  },
+  beforeMount() {
+    this.getUserName();
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.welcome{
-  display:flex;
+.welcome {
+  display: flex;
   flex-direction: column;
   align-items: center;
 }
-.welcome > *{
-  width:100%;
+.welcome > * {
+  width: 100%;
 }
-#welcome__actu{
-  width:80%;
-  min-height:10rem;
-  margin:auto;
+#welcome__actu {
+  width: 80%;
+  min-height: 10rem;
+  margin: auto;
   border: 1px solid var(--color-primary);
 }
 </style>
