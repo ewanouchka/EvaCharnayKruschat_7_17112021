3c389fafa93af777d24042349f5e25379f417fb7
ewanouchka <ewanouchka@gmail.com>
2021-12-28 11:56:49 +0100

    formatage code

--- grouporama-app/src/components/Popup.vue
+++ grouporama-app/src/components/Popup.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="popup-container" id="popup">
     <div class="popup-bloc">
-    <div class="popup-bloc__msg">{{ msg }}</div>
-    <pre class="popup-bloc__detail">{{ detail }}</pre>
-    <button class="button close" @click="$emit('close')" id="close-popup">Fermer</button>
+      <div class="popup-bloc__msg">{{ msg }}</div>
+      <pre class="popup-bloc__detail">{{ detail }}</pre>
+      <button class="button close" @click="$emit('close')" id="close-popup">
+        Fermer
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Popup',
-  props: ['msg', 'detail'],
-}
+  name: "Popup",
+  props: ["msg", "detail"],
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -24,7 +26,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  position:absolute;
+  position: absolute;
   top: 0;
   left: 0;
   z-index: 50;
@@ -37,19 +39,21 @@ export default {
   border-radius: 3rem;
   box-shadow: 0 0 0.5rem var(--color-primary);
   font-family: var(--font-text);
+  color: var(--color-dark);
   text-align: center;
   padding: 2rem;
 }
 
-.popup-bloc__msg{
- font-weight:bold;
+.popup-bloc__msg {
+  font-weight: bold;
 }
 
-.popup-bloc__detail, pre{
- text-align: justify;
- font-weight:normal;
- font-family: inherit;
- width:100%;
- white-space:pre-wrap;
+.popup-bloc__detail,
+pre {
+  text-align: justify;
+  font-weight: normal;
+  font-family: inherit;
+  width: 100%;
+  white-space: pre-wrap;
 }
 </style>
