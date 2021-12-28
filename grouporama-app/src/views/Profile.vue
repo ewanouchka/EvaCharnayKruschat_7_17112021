85d7ca8860c26c0bd6ba34c8a721b6e599158847
ewanouchka <ewanouchka@gmail.com>
2021-12-28 11:18:31 +0100

    insert form + boutons modify/suppr (mq fonctions)

--- grouporama-app/src/views/Profile.vue
+++ grouporama-app/src/views/Profile.vue
@@ -5,8 +5,100 @@
       src="../assets/icon-above-font.svg"
       class="home__logo"
     />
-    <h1>{{ userFirstName }} {{ userLastName }}</h1>
-    <p>{{ userEmail }}</p>
+    <section class="profile__card">
+      <div v-if="isEditVisible === true" id="profile-edit">
+        <h1>Editer votre profil</h1>
+        <form id="profile-form">
+          <label for="Nom" class="form-label">Votre nom :</label>
+          <input
+            placeholder="Dupont"
+            name="Name"
+            id="Name"
+            class="form-input"
+            type="text"
+            required
+            pattern="^[àáâãäåçèéêëìíîïðòóôõöùúûüýÿa-zA-Z '-]{2,}$"
+          />
+          <span class="error-visible" id="error-message-Name"></span>
+
+          <label for="Prénom" class="form-label"
+            >Votre prénom :
+            <span class="error-visible" id="error-message-Surname"></span>
+          </label>
+          <input
+            placeholder="Dominique"
+            name="Surname"
+            id="Surname"
+            class="form-input"
+            type="text"
+            required
+            pattern="^[àáâãäåçèéêëìíîïðòóôõöùúûüýÿa-zA-Z '-]{2,}$"
+          />
+
+          <label for="Email" class="form-label"
+            >Votre e-mail :
+            <span class="error-visible" id="error-message-Email"></span>
+          </label>
+          <input
+            placeholder="contact@groupomania.com"
+            name="Email"
+            id="Email"
+            class="form-input"
+            type="email"
+            required
+            pattern="^[a-zA-Z0-9]+[a-zA-Z._-]*@{1}[a-zA-Z0-9]+[.]{1}[a-zA-Z]{2,}$"
+          />
+
+          <label for="Password" class="form-label"
+            >Votre mot de passe :
+            <span class="error-visible" id="error-message-Password"></span>
+          </label>
+          <input
+            placeholder="123456AzErTy*"
+            name="Password"
+            id="Password"
+            class="form-input"
+            type="password"
+            required
+            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&()_+=-])[A-Za-z\d@$!%*?&()_+=-]{8,}$"
+          />
+
+          <label for="RepeatPassword" class="form-label"
+            >Répétez votre mot de passe :
+            <span
+              class="error-visible"
+              id="error-message-Repeat-Password"
+            ></span>
+          </label>
+          <input
+            placeholder="123456AzErTy*"
+            name="Repeat-Password"
+            id="Repeat-Password"
+            class="form-input"
+            type="password"
+            required
+            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&()_+=-])[A-Za-z\d@$!%*?&()_+=-]{8,}$"
+          />
+
+          <button @click.prevent="closeEdit" class="button" id="edit-profile">
+            Enregistrer les modifications
+          </button>
+        </form>
+      </div>
+
+      <div v-else id="profile-content">
+        <h1>{{ userFirstName }} {{ userLastName }}</h1>
+        <p>{{ userEmail }}</p>
+      </div>
+
+      <button @click.prevent="showEdit" class="button" id="show-edit">
+        Modifier votre profil
+      </button>
+
+      <button @click.prevent="showEdit" class="button" id="show-edit">
+        Supprimer votre compte
+      </button>
+    </section>
   </div>
 </template>
 
@@ -20,9 +112,16 @@ export default {
       userFirstName: this.userSurname,
       userLastName: this.userName,
       userEmail: this.email,
+      isEditVisible: false,
     };
   },
   methods: {
+    showEdit() {
+      this.isEditVisible = true;
+    },
+    closeEdit() {
+      this.isEditVisible = false;
+    },
     getUserProfile() {
       const userToken = JSON.parse(localStorage.getItem("userAuth")).token;
       (async () => {
@@ -35,7 +134,6 @@ export default {
             },
           });
           const userProfileJSON = await userProfile.json();
-          console.log(userProfileJSON);
 
           if (userProfileJSON) {
             this.userFirstName = userProfileJSON.first_name;
@@ -57,4 +155,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+#profile-content {
+  border: 2px outset var(--color-secondary);
+  border-radius: 8px;
+}
+
+#profile-form {
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  flex-wrap: wrap;
+  max-width: 40rem;
+  background: var(--color-primary-transparent);
+  border-radius: 0.5rem;
+}
 </style>
