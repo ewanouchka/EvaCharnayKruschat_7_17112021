38748fb6c07c6531083f6417b61d26d1be3d5e1f
ewanouchka <ewanouchka@gmail.com>
2021-12-28 11:17:34 +0100

    formatage

--- grouporama-app/src/views/Home.vue
+++ grouporama-app/src/views/Home.vue
@@ -1,78 +1,50 @@
 <template>
   <div class="home">
-    <img alt="logo above Groupomania" src="../assets/icon-above-font.svg" class="home__logo">
-    <section v-if="isLoggedIn === false"><Login/></section>
+    <img
+      alt="logo above Groupomania"
+      src="../assets/icon-above-font.svg"
+      class="home__logo"
+    />
+    <section v-if="isLoggedIn === false"><Login /></section>
     <section v-else><Welcome /></section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Login from '@/components/Login.vue'
-import Welcome from '@/components/Welcome.vue'
+import Login from "@/components/Login.vue";
+import Welcome from "@/components/Welcome.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Login, Welcome
-  }, 
-  data: 
-    function() {
-      return {
-        isLoggedIn: 'false'
-      };
-    },
+    Login,
+    Welcome,
+  },
+  data: function () {
+    return {
+      isLoggedIn: "false",
+    };
+  },
   methods: {
-    checkAuth () {
-      try { 
+    checkAuth() {
+      try {
         const verify = JSON.parse(localStorage.getItem("userAuth"));
         if (!verify || verify.token == undefined) {
-          return this.isLoggedIn = false;
+          return (this.isLoggedIn = false);
         } else {
-          return this.isLoggedIn = true;
+          return (this.isLoggedIn = true);
         }
+      } catch (error) {
+        console.log(error);
       }
-      catch (error) { console.log(error);}
-    } 
-  }, beforeMount(){
+    },
+  },
+  beforeMount() {
     this.checkAuth();
- }
-}
+  },
+};
 </script>
 
 <style lang="scss">
-.home__logo {
-  width:40rem;
-  height:25rem;
-  object-fit: cover;
-  }
-
-@media all and (min-width: 426px) and (max-width: 767px) {
-.home__logo {
-  width:400px;
-  height:15rem;
-  }
-}
-
-@media all and (min-height: 641px) and (max-height: 823px) {
-.home__logo {
-  width:400px;
-  height:15rem;
-  }
-}
-
-@media all and (max-width: 425px) {
-.home__logo {
-  width:270px;
-  height:9rem;
-  }
-}
-
-@media all and (max-height: 640px) {
-.home__logo {
-  width:270px;
-  height:9rem;
-  }
-}
-
 </style>
