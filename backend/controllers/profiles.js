c745b14e44d4ec596d38341483a0031d0bf24de7
ewanouchka <ewanouchka@gmail.com>
2021-12-29 18:14:04 +0100

    suppr doublon auth fonction updateUserProfile

--- backend/controllers/profiles.js
+++ backend/controllers/profiles.js
@@ -31,70 +31,41 @@ exports.getUserProfile = (req, res, next) => {
 
 // fonction modification du profil utilisateur
 exports.updateUserProfile = (req, res, next) => {
-  const headerAuth = req.headers["authorization"];
-  let userToken = "";
-
-  if (headerAuth != null) {
-    userToken = headerAuth.replace("Bearer ", "");
-  }
-
-  const getUserId = () => {
-    let userId = -1;
-
-    if (userToken != null) {
-      try {
-        const jwtTokenInfo = jwt.verify(userToken, dbSecretToken);
-        if (jwtTokenInfo != null) {
-          userId = jwtTokenInfo.userId;
-        }
-      } catch (error) {
-        res.status(500).json({ error: "l'id utilisateur n'a pas pu être récupéré" });
-      }
-    }
-    return userId;
-  };
-
-  const userId = getUserId(userToken);
-
-  if (userId < 0) {
-    return res.status(400).json({ error: "token non reconnu" });
-  } else {
-    models.User.findOne({
-      attributes: ["id", "email", "first_name", "last_name", "password"],
-      where: { id: userId },
-    })
-      .then(function (userFound) {
-        if (userFound) {
-          const updateUser = (newPass) => {
-            userFound
-              .update({
-                first_name: req.body.first_name ? req.body.first_name : userFound.first_name,
-                last_name: req.body.last_name ? req.body.last_name : userFound.last_name,
-                email: req.body.email ? req.body.email : userFound.email,
-                password: newPass,
-              })
-              .then(function (userFound) {
-                return res.status(201).json(userFound);
-              })
-              .catch(function (err) {
-                res.status(500).json({ error: "échec de la mise à jour utilisateur" });
-              });
-          };
-          if (req.body.password) {
-            bcrypt.hash(req.body.password, 10, function (err, hash) {
-              updateUser(hash);
+  models.User.findOne({
+    attributes: ["id", "email", "first_name", "last_name", "password"],
+    where: { id: req.body.userId },
+  })
+    .then(function (userFound) {
+      if (userFound) {
+        const updateUser = (newPass) => {
+          userFound
+            .update({
+              first_name: req.body.first_name ? req.body.first_name : userFound.first_name,
+              last_name: req.body.last_name ? req.body.last_name : userFound.last_name,
+              email: req.body.email ? req.body.email : userFound.email,
+              password: newPass,
+            })
+            .then(function (userFound) {
+              return res.status(201).json(userFound);
+            })
+            .catch(function (err) {
+              res.status(500).json({ error: "échec de la mise à jour utilisateur" });
             });
-          } else {
-            updateUser(userFound.password);
-          }
+        };
+        if (req.body.password) {
+          bcrypt.hash(req.body.password, 10, function (err, hash) {
+            updateUser(hash);
+          });
         } else {
-          res.status(404).json({ error: "utilisateur inconnu" });
+          updateUser(userFound.password);
         }
-      })
-      .catch(function () {
-        res.status(500).json({ error: "échec de l'accès à l'utilisateur" });
-      });
-  }
+      } else {
+        res.status(404).json({ error: "utilisateur inconnu" });
+      }
+    })
+    .catch(function () {
+      res.status(500).json({ error: "échec de l'accès à l'utilisateur" });
+    });
 };
 
 // fonction suppression d'un utilisateur
