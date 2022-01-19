6b99069e71cf32104a0553639436b0c048041b9c
ewanouchka <ewanouchka@gmail.com>
2022-01-19 14:19:45 +0100

    migration corrective après tentative modif UserId

--- backend/db/migrations/20220117145421-create-message.js
+++ backend/db/migrations/20220117145421-create-message.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      idUSERS: {
+      UserId: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
