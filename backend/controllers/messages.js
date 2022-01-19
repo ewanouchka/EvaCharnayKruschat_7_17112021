16f96a8ae19e82ae04f8cc1278e5e33482e23df9
ewanouchka <ewanouchka@gmail.com>
2022-01-19 14:23:23 +0100

    CRUD mise en place

--- backend/controllers/messages.js
+++ backend/controllers/messages.js
@@ -1,7 +1,3 @@
-// import des modules pour sécurisation des données
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
 // import des models
 const models = require("../db/models");
 
@@ -30,16 +26,121 @@ exports.getMessages = (req, res, next) => {
       }
     })
     .catch(function (err) {
-      console.log(err);
       res.status(500).json({ error: "la demande est incorrecte." });
     });
 };
 
 // fonction envoi d'un nouveau post
-exports.sendMessage = (req, res, next) => {};
+exports.sendMessage = (req, res, next) => {
+  // on bloque si des champs requis sont manquants
+  if (!req.body.title || !req.body.content) {
+    res.status(400).json({ error: "Certains champs ne sont pas remplis." });
+  }
+
+  // on bloque aussi si les champs renseignés sont trop courts
+  if (req.body.title <= 3 || req.body.content <= 3) {
+    return res.status(400).json({ error: "Le contenu est trop court." });
+  }
+
+  // si c'est ok, on crée le message
+  models.User.findOne({
+    where: { id: req.body.userId },
+  })
+    .then(function (userFound) {
+      if (userFound) {
+        const newMessage = models.Message.create({
+          UserId: req.body.userId,
+          title: req.body.title,
+          content: req.body.content,
+          attachment: null,
+          likes: 0,
+        }) // revoir pour intégrer l'ajout d'une image
+
+          .then((newMessage) => {
+            res.status(201).json({ message: "Message enregistré !" });
+          })
+          .catch((err) => {
+            return res.status(500).json({ error: "Erreur d'enregistrement du message." });
+          });
+      } else {
+        res.status(404).json({ error: "Utilisateur non trouvé." });
+      }
+    })
+    .catch(function (err) {
+      return res.status(500).json({ error: err });
+    });
+};
 
 // fonction modification d'un post
-exports.updateMessage = (req, res, next) => {};
+exports.updateMessage = (req, res, next) => {
+  // on bloque si des champs requis sont manquants
+  if (!req.body.title || !req.body.content) {
+    res.status(400).json({ error: "Certains champs ne sont pas remplis." });
+  }
+
+  // on bloque aussi si les champs renseignés sont trop courts
+  if (req.body.title <= 3 || req.body.content <= 3) {
+    return res.status(400).json({ error: "Le contenu est trop court." });
+  }
+
+  // si c'est ok, on modifie le message
+  models.Message.findOne({
+    where: { id: req.params.messageId },
+  })
+    .then(function (messageFound) {
+      if (messageFound.UserId != req.body.userId) {
+        // modifier le if pour prendre en compte les droits admin
+        res.status(403).json({ error: "utilisateur non autorisé" });
+        return;
+      }
+      if (messageFound) {
+        messageFound
+          .update({
+            title: req.body.title ? req.body.title : messageFound.title,
+            content: req.body.content ? req.body.content : messageFound.content,
+            attachment: req.body.attachment ? req.body.attachment : messageFound.attachment,
+          })
+          .then(function (messageFound) {
+            return res.status(201).json({ message: "Message modifié !" });
+          })
+          .catch(function (err) {
+            res.status(500).json({ error: "échec de la mise à jour du message" });
+          });
+      } else {
+        res.status(404).json({ error: "Message non trouvé." });
+      }
+    })
+    .catch(function (err) {
+      return res.status(500).json({ error: err });
+    });
+};
 
 // fonction suppression d'un post
-exports.deleteMessage = (req, res, next) => {};
+exports.deleteMessage = (req, res, next) => {
+  models.Message.findOne({
+    attributes: ["id", "UserId"],
+    where: { id: req.params.messageId },
+  })
+    .then(function (messageFound) {
+      if (messageFound.UserId != req.body.userId) {
+        // modifier le if pour prendre en compte les droits admin
+        res.status(403).json({ error: "utilisateur non autorisé" });
+        return;
+      }
+      if (messageFound) {
+        messageFound
+          .destroy({ where: { id: req.params.messageId } })
+          .then(function () {
+            return res.status(201).json({ message: "message supprimé !" });
+          })
+          .catch(function (err) {
+            res.status(500).json({ error: "échec de la suppression du message" });
+          });
+      } else {
+        return res.status(404).json({ error: "message non trouvé" });
+      }
+    })
+    .catch(function () {
+      res.status(500).json({ error: "échec de l'accès au message" });
+    });
+};
