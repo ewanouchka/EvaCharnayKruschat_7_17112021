39cfd10742a84e55b399bbac606525ba09e685f5
ewanouchka <ewanouchka@gmail.com>
2022-01-21 11:19:47 +0100

    ajout res.locals + export et simplif° fctns

--- backend/controllers/profiles.js
+++ backend/controllers/profiles.js
@@ -1,65 +1,71 @@
 // import des modules pour sécurisation des données
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 
 // import des models
 const models = require("../db/models");
 
 // fonction accès au profil utilisateur
 exports.getUserProfile = (req, res, next) => {
+  // constantes
+  const userId = res.locals.userId;
+  const isAdmin = res.locals.isAdmin;
+
   models.User.findOne({
-    attributes: ["id", "email", "first_name", "last_name"],
     where: { id: req.params.id },
   })
-    .then(function (user) {
-      if (user) {
-        res.status(201).json(user);
-      } else {
-        res.status(404).json({ error: "utilisateur inconnu" });
+    .then((userFound) => {
+      if (userFound === null) {
+        return res.status(404).json({ error: "Profil non trouvé" });
       }
+      if (!isAdmin && userFound.id != userId) {
+        return res.status(403).json({ error: "Utilisateur non autorisé" });
+      }
+      return res.status(201).json(userFound);
     })
-    .catch(function () {
-      res.status(500).json({ error: "échec de l'accès à l'utilisateur" });
+    .catch(() => {
+      return res.status(500).json({ error: "échec de l'accès à l'utilisateur" });
     });
 };
 
 // fonction modification du profil utilisateur
+const updateUser = (req, res, userFound, newPass, next) => {
+  userFound
+    .update({
+      first_name: req.body.first_name ? req.body.first_name : userFound.first_name,
+      last_name: req.body.last_name ? req.body.last_name : userFound.last_name,
+      email: req.body.email ? req.body.email : userFound.email,
+      password: newPass,
+    })
+    .then((userFound) => {
+      return res.status(201).json(userFound);
+    })
+    .catch(() => {
+      res.status(500).json({ error: "Echec de la mise à jour utilisateur" });
+    });
+};
+
 exports.updateUserProfile = (req, res, next) => {
+  // constantes
+  const userId = res.locals.userId;
+  const isAdmin = res.locals.isAdmin;
+
   models.User.findOne({
-    attributes: ["id", "email", "first_name", "last_name", "password"],
     where: { id: req.params.id },
   })
-    .then(function (userFound) {
-      const updateUser = (newPass) => {
-        userFound
-          .update({
-            first_name: req.body.first_name ? req.body.first_name : userFound.first_name,
-            last_name: req.body.last_name ? req.body.last_name : userFound.last_name,
-            email: req.body.email ? req.body.email : userFound.email,
-            password: newPass,
-          })
-          .then(function (userFound) {
-            return res.status(201).json(userFound);
-          })
-          .catch(function (err) {
-            res.status(500).json({ error: "échec de la mise à jour utilisateur" });
-          });
-      };
-
-      if (userFound) {
-        if (req.body.password) {
-          bcrypt.hash(req.body.password, 10, function (err, hash) {
-            updateUser(hash);
-          });
-        } else {
-          updateUser(userFound.password);
-        }
+    .then((userFound) => {
+      if (!isAdmin && userFound.id != userId) {
+        return res.status(403).json({ error: "Utilisateur non autorisé" });
+      }
+      if (req.body.password) {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          updateUser(req, res, userFound, hash);
+        });
       } else {
-        res.status(404).json({ error: "utilisateur inconnu" });
+        updateUser(req, res, userFound, userFound.password);
       }
     })
-    .catch(function () {
-      res.status(500).json({ error: "échec de l'accès à l'utilisateur" });
+    .catch(() => {
+      res.status(500).json({ error: "Echec de l'accès à l'utilisateur" });
     });
 };
 
@@ -69,27 +75,23 @@ exports.deleteUserProfile = (req, res, next) => {
     attributes: ["id", "password"],
     where: { id: req.params.id },
   })
-    .then(function (userFound) {
-      if (userFound) {
-        bcrypt.compare(req.body.password, userFound.password, function (err, resBcrypt) {
-          if (resBcrypt) {
-            userFound
-              .destroy({ where: { id: req.params.id } })
-              .then(function () {
-                return res.status(201).json({ message: "utilisateur supprimé !" });
-              })
-              .catch(function (err) {
-                res.status(500).json({ error: "échec de la suppression utilisateur" });
-              });
-          } else {
-            return res.status(403).json({ error: "Mot de passe incorrect !" });
-          }
-        });
-      } else {
-        res.status(404).json({ error: "utilisateur inconnu" });
-      }
+    .then((userFound) => {
+      bcrypt.compare(req.body.password, userFound.password, (err, resBcrypt) => {
+        if (resBcrypt) {
+          userFound
+            .destroy({ where: { id: req.params.id } })
+            .then(() => {
+              return res.status(201).json({ message: "Utilisateur supprimé !" });
+            })
+            .catch(() => {
+              res.status(500).json({ error: "Echec de la suppression utilisateur" });
+            });
+        } else {
+          return res.status(403).json({ error: "Mot de passe incorrect !" });
+        }
+      });
     })
-    .catch(function () {
-      res.status(500).json({ error: "échec de l'accès à l'utilisateur" });
+    .catch(() => {
+      res.status(500).json({ error: "Echec de l'accès à l'utilisateur" });
     });
 };
