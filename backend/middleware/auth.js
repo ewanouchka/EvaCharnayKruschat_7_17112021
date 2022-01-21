a1d6de05b33c2bceff5bd9911c216ca05541a2f9
ewanouchka <ewanouchka@gmail.com>
2022-01-21 11:20:52 +0100

    modif fctn: ajout res.locals + vérif user en base

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,17 @@
 // import des modules pour sécurisation des données
 const jwt = require("jsonwebtoken");
 
+// import des models
+const models = require("../db/models");
+
 // import variables environnementales
 const dotEnv = require("dotenv");
 dotEnv.config();
 const dbSecretToken = process.env.TOKEN_SECRET;
 
 // fonction authentification : on vérifie la connexion utilisateur
+
+// voir pour vérifier si l'utilisateur est existant en base
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
@@ -14,21 +19,26 @@ module.exports = (req, res, next) => {
     const userId = decodedToken.userId;
     const isAdmin = decodedToken.isAdmin;
 
-    // si l'utilisateur n'est pas connecté ou s'il n'est pas le propriétaire du compte, du message ou du commentaire
-    // revoir la condition pour inclure les droits admin
-
     if (
-      !userId || // utilisateur non connecté ou token non correctement décodé
-      (req.params.id && req.params.id != userId) || // défaut de concordance du paramètre id pour les profils utilisateurs avec l'utilisateur connecté
-      (req.body.userId && req.body.userId != userId) || // défaut de concordance du posteur de la requête avec l'utilisateur connecté
-      (req.body.messageOwner && req.body.messageOwner != userId) // défaut de concordance entre le créateur du message et l'utilisateur connecté
+      !userId // utilisateur non connecté ou token non correctement décodé
     ) {
-      res.status(403).json({ message: "utilisateur non autorisé" });
-    } else {
-      next();
-      return userId, isAdmin;
+      return res.status(403).json({ message: "Utilisateur non authorisé." });
     }
-  } catch (error) {
-    res.status(401).json({ error: "l'authentification a échoué" });
+
+    // on vérifie que le userId décodé est bien présent en base de données
+    models.User.findOne({
+      attributes: ["id"],
+      where: { id: userId },
+    })
+      .then(() => {
+        res.locals.userId = userId;
+        res.locals.isAdmin = isAdmin;
+        next();
+      })
+      .catch(() => {
+        return res.status(404).json({ error: "Cet utilisateur n'existe pas." });
+      });
+  } catch (e) {
+    return res.status(401).json({ error: "L'authentification a échoué." });
   }
 };
