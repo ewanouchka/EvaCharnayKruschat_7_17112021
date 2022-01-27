0157de7c2e818f7f90c50dada41e554acab461dc
ewanouchka <ewanouchka@gmail.com>
2022-01-28 00:08:51 +0100

    ajout route getOneMessage

--- backend/routes/messages.js
+++ backend/routes/messages.js
@@ -8,8 +8,9 @@ const auth = require("../middleware/auth");
 // on récupère les fonctions dans les controllers > users
 const messageCtrl = require("../controllers/messages");
 
-// on crée les routes -- A REVOIR
+// on crée les routes
 router.get("/", auth, messageCtrl.getMessages);
+router.get("/:messageId", auth, messageCtrl.getOneMessage);
 router.post("/", auth, messageCtrl.sendMessage);
 router.put("/:messageId", auth, messageCtrl.updateMessage);
 router.delete("/:messageId", auth, messageCtrl.deleteMessage);
